a8f22afa1a0dd20772b294576a9c6a114947686d
Alex Baier <abaier@uni-koblenz.de>
2017-12-02 15:21:34 +0100

    Replaced layout classes with decorated functions.

--- vtna/layout.py
+++ vtna/layout.py
@@ -1,7 +1,5 @@
-__all__ = ['FlexibleSpringLayout', 'StaticSpringLayout']
+__all__ = ['flexible_spring_layout', 'static_spring_layout']
 
-import abc
-import copy
 import typing as typ
 
 import networkx as nx
@@ -13,84 +11,42 @@ import vtna.utility as util
 Point = typ.Tuple[float, float]
 
 
-class Layout(util.Describable, metaclass=abc.ABCMeta):
-    def __init__(self, temp_graph: vtna.graph.TemporalGraph):
-        self.__temp_graph = temp_graph
-        self.__layout = None  # type: typ.List[typ.Dict[int, Point]]
-
-    @abc.abstractmethod
-    def compute(self):
-        pass
-
-    def __getitem__(self, time_step: typ.Union[slice, int]) -> typ.Dict[int, Point]:
-        if self.__layout is None:
-            raise NoComputedLayoutError()
-        return copy.deepcopy(self.__layout[time_step])
-
-    def __setitem__(self, time_step: int, mapping: typ.Dict[int, Point]):
-        if self.__layout is None:
-            raise NoComputedLayoutError()
-        self.__layout[time_step].update(mapping)
-
-    @abc.abstractmethod
-    def is_static(self) -> bool:
-        pass
+def is_static(static: bool):
+    """Decorator, adds is_static attribute to layout function."""
+    def decorator(func):
+        func.is_static = static
+        return func
+    return decorator
 
 
-class FlexibleLayout(Layout, metaclass=abc.ABCMeta):
-    def compute(self):
-        self.__layout = self.__compute_flexible_layout()
-
-    def is_static(self) -> bool:
-        return False
-
-    @abc.abstractmethod
-    def __compute_flexible_layout(self) -> typ.List[typ.Dict[int, Point]]:
-        pass
+def name(n: str):
+    """Decorator, adds name attribute to layout function."""
+    def decorator(func):
+        func.name = n
+        return func
+    return decorator
 
 
-class StaticLayout(Layout, metaclass=abc.ABCMeta):
-    def compute(self):
-        layout = self.__compute_static_layout()
-        self.__layout = [layout.copy() for _ in range(len(self.__temp_graph))]
-
-    def is_static(self) -> bool:
-        return True
-
-    @abc.abstractmethod
-    def __compute_static_layout(self) -> typ.Dict[int, Point]:
-        pass
+def description(d: str):
+    """Decorator, adds description attribute to layout function."""
+    def decorator(func):
+        func.description = d
+        return func
+    return decorator
 
 
-class FlexibleSpringLayout(FlexibleLayout):
-    def __init__(self, temp_graph: vtna.graph.TemporalGraph):
-        super(FlexibleSpringLayout, self).__init__(temp_graph)
-
-    def __compute_flexible_layout(self) -> typ.List[typ.Dict[int, Point]]:
-        return [nx.spring_layout(util.graph2networkx(util.graph2networkx(graph)), dim=2, weight=None)
-                for graph in self.__temp_graph]
-
-    def get_name(self) -> str:
-        return 'Flexible Spring Layout'
-
-    def get_description(self) -> str:
-        return 'Basic Spring layout with one individual layout per timestep'
+@is_static(False)
+@name('Flexible Spring Layout')
+@description('Basic Spring layout with one individual layout per time step')
+def flexible_spring_layout(temp_graph: vtna.graph.TemporalGraph) -> typ.List[typ.Dict[int, Point]]:
+    return [nx.spring_layout(util.graph2networkx(util.graph2networkx(graph)), dim=2, weight=None)
+            for graph in temp_graph]
 
 
-class StaticSpringLayout(StaticLayout):
-    def __init__(self, temp_graph: vtna.graph.TemporalGraph):
-        super(StaticSpringLayout, self).__init__(temp_graph)
+@is_static(True)
+@name('Static Spring Layout')
+@description('Basic Spring layout which ensures static node position by aggregating all observations')
+def static_spring_layout(temp_graph: vtna.graph.TemporalGraph)  -> typ.List[typ.Dict[int, Point]]:
+    layout = nx.spring_layout(util.temporal_graph2networkx(temp_graph), dim=2, weight=None)
+    return [layout.copy() for _ in range(len(temp_graph))]
 
-    def __compute_static_layout(self):
-        return nx.spring_layout(util.temporal_graph2networkx(self.__temp_graph), dim=2, weight=None)
-
-    def get_name(self) -> str:
-        return 'Static Spring Layout'
-
-    def get_description(self) -> str:
-        return 'Basic Spring layout which ensures static node position by aggregating all observations'
-
-
-class NoComputedLayoutError(Exception):
-    def __init__(self):
-        self.message = 'No layout exists. Call compute() to create the layout.'
