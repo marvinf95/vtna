9e08de993b424207acce84ff78f39f67d55db800
Marvin Forster <marvinforster@uni-koblenz.de>
2017-12-07 23:17:29 +0100

    added comments to filter.py

--- vtna/filter.py
+++ vtna/filter.py
@@ -2,6 +2,7 @@ import typing as typ
 
 import vtna.graph
 
+
 def name(n: str):
     """Decorator, adds name attribute to layout function."""
     def decorator(func):
@@ -21,6 +22,16 @@ def description(d: str):
 @name('Filter Attributes')
 @description('Filter for special values in attributes')
 def filter_attributes(temp_graph: vtna.graph.TemporalGraph, name: str, value: vtna.graph.AttributeValue) -> typ.List[int]:
+    """
+            Retrieves a temporal graph, a attribute name and value.
+
+            Args:
+                temp_graph: Temporal graph with all nodes and attributes.
+                name: One global attribute name.
+                value: Value for the attribute.
+            Returns:
+                List of node_ids that contain the attribute with the given value.
+            """
     filter_list = []
     for i in temp_graph.get_nodes():
         if i.get_global_attribute(name) == value:
