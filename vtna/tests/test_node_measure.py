a1c5f28a6f3faf5f1735d41c2208ac51a85c193d
Philipp Töws <ptoews@uni-koblenz.de>
2017-12-07 01:00:15 +0100

    Add tests for full coverage

--- vtna/tests/test_node_measure.py
+++ vtna/tests/test_node_measure.py
@@ -1,8 +1,11 @@
 import unittest
 
+from nose.tools import raises
+
 import vtna.node_measure as nome
 import vtna.data_import as dimp
 import vtna.graph as graph
+import coverage
 
 
 class TestCentralityMeasures(unittest.TestCase):
@@ -13,37 +16,54 @@ class TestCentralityMeasures(unittest.TestCase):
 
     def test_local_degree_centrality(self):
         degree_centrality = nome.LocalDegreeCentrality(self._temp_graph)
+        self.assertIsNotNone(degree_centrality.get_description())
         degree_centrality.add_to_graph()
         for node in self._temp_graph.get_nodes():
             dc = node.get_local_attribute(degree_centrality.get_name(), 1)
 
     def test_global_degree_centrality(self):
         degree_centrality = nome.GlobalDegreeCentrality(self._temp_graph)
+        self.assertIsNotNone(degree_centrality.get_description())
         degree_centrality.add_to_graph()
         for node in self._temp_graph.get_nodes():
             dc = node.get_global_attribute(degree_centrality.get_name())
 
     def test_local_betweenness_centrality(self):
         betw_centrality = nome.LocalBetweennessCentrality(self._temp_graph)
+        self.assertIsNotNone(betw_centrality.get_description())
         betw_centrality.add_to_graph()
         for node in self._temp_graph.get_nodes():
             bc = node.get_local_attribute(betw_centrality.get_name(), 1)
 
     def test_global_betweenness_centrality(self):
         betw_centrality = nome.GlobalBetweennessCentrality(self._temp_graph)
+        self.assertIsNotNone(betw_centrality.get_description())
         betw_centrality.add_to_graph()
         for node in self._temp_graph.get_nodes():
             bc = node.get_global_attribute(betw_centrality.get_name())
 
     def test_local_closeness_centrality(self):
         clsns_centrality = nome.LocalClosenessCentrality(self._temp_graph)
+        self.assertIsNotNone(clsns_centrality.get_description())
         clsns_centrality.add_to_graph()
         for node in self._temp_graph.get_nodes():
             cc = node.get_local_attribute(clsns_centrality.get_name(), 1)
 
     def test_global_closeness_centrality(self):
         clsns_centrality = nome.GlobalClosenessCentrality(self._temp_graph)
+        self.assertIsNotNone(clsns_centrality.get_description())
         clsns_centrality.add_to_graph()
         for node in self._temp_graph.get_nodes():
             cc = node.get_global_attribute(clsns_centrality.get_name())
 
+    @raises(TypeError)
+    def test_lbc_getitem_invalid_parameter(self):
+        nome.LocalDegreeCentrality(self._temp_graph).__getitem__("NotAnInteger")
+
+    @raises(TypeError)
+    def test_gcc_getitem_invalid_parameter(self):
+        nome.GlobalClosenessCentrality(self._temp_graph).__getitem__("NotAnInteger")
+
+    @raises(TypeError)
+    def test_lbc_init_invalid_parameter(self):
+        nome.LocalDegreeCentrality(self._temp_graph).__init__("NotATemporalGraph")
